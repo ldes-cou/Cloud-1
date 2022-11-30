6178d14b26c176fc92e10fc4d8d2a8b2ef027ac3
ldes-cou <ldes-cou@student.42.fr>
2022-11-30 12:38:32 +0100

    adding DNS

--- deploy_docker/files/inception/requirements/wordpress/conf/script.sh
+++ deploy_docker/files/inception/requirements/wordpress/conf/script.sh
@@ -6,7 +6,7 @@ if [ ! -f /var/www/html/wp-config.php  ]; then
     done
     mkdir -p /var/www/html && cd /var/www/html/ && wp core download --allow-root \
     && wp config create --dbname=$MYSQL_DB --dbuser=$MYSQL_USER --dbpass=$MYSQL_USER_PASSWORD --dbhost=$WP_DB_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root \
-    && wp core install --url=51.159.179.137 --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_MAIL --skip-email --allow-root \
+    && wp core install --url=cloud1-is-clacked-on-the-floor.duckdns.org --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_MAIL --skip-email --allow-root \
     && wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_PASSWORD --allow-root \
     && wp theme install $WP_THEME --activate --allow-root 
 else
